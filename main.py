8c6450696cb64a4829e89ea70289d408f3bcf540
Charldave <redito.c17@gmail.com>
2025-06-27 21:46:54 +0800

    Pygame window and game settings

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+width = 500
+height = 500
+screen_size = (width, height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Car Game')
