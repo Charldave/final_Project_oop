b8b98b9e9e7fd596bd7950fb4071a7f2ef8d4886
Charldave <redito.c17@gmail.com>
2025-06-27 22:10:24 +0800

    vehicle class

--- car_game.py
+++ car_game.py
@@ -40,3 +40,16 @@ fps = 120
 gameover = False
 speed = 2
 score = 0
+
+class Vehicle(pygame.sprite.Sprite):
+    
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        image_scale = 45 / image.get_rect().width
+        new_width = image.get_rect().width * image_scale
+        new_height = image.get_rect().height * image_scale
+        self.image = pygame.transform.scale(image, (new_width, new_height))
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
