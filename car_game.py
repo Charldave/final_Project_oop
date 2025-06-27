dd08c3aeba0bcab90e13b441460bd96788978233
Charldave <redito.c17@gmail.com>
2025-06-27 22:22:26 +0800

    main game loop and background screen

--- car_game.py
+++ car_game.py
@@ -74,3 +74,46 @@ for image_filename in image_filenames:
     
 crash = pygame.image.load('images/crash.png')
 crash_rect = crash.get_rect()
+
+running = True
+while running:
+    
+    clock.tick(fps)
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+            
+        if event.type == KEYDOWN:
+            
+            if event.key == K_LEFT and player.rect.center[0] > left_lane:
+                player.rect.x -= 100
+            elif event.key == K_RIGHT and player.rect.center[0] < right_lane:
+                player.rect.x += 100
+                
+            for vehicle in vehicle_group:
+                if pygame.sprite.collide_rect(player, vehicle):
+                    
+                    gameover = True
+                    
+                    if event.key == K_LEFT:
+                        player.rect.left = vehicle.rect.right
+                        crash_rect.center = [player.rect.left, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+                    elif event.key == K_RIGHT:
+                        player.rect.right = vehicle.rect.left
+                        crash_rect.center = [player.rect.right, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+            
+            
+    screen.fill(green)
+    
+    pygame.draw.rect(screen, gray, road)
+    
+    pygame.draw.rect(screen, yellow, left_edge_marker)
+    pygame.draw.rect(screen, yellow, right_edge_marker)
+    
+    lane_marker_move_y += speed * 2
+    if lane_marker_move_y >= marker_height * 2:
+        lane_marker_move_y = 0
+    for y in range(marker_height * -2, height, marker_height * 2):
+        pygame.draw.rect(screen, white, (left_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
+        pygame.draw.rect(screen, white, (center_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
